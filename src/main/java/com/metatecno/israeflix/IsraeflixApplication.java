b2a8caac7ab625bc04c3e80dba416f5deadec3a4
Alex Cruz <alexbenicio@hotmail.com.br>
2022-03-29 16:42:09 -0300

    Configurado projeto Maven

---
+++ src/main/java/com/metatecno/israeflix/IsraeflixApplication.java
@@ -0,0 +1,13 @@
+package com.metatecno.israeflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsraeflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsraeflixApplication.class, args);
+	}
+
+}
